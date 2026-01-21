49107ef4106ac99e3023d3f79a3c7cc3cc091911
Claudio-BF <claudiobf@uchicago.edu>
2026-01-21 01:13:59 -0600

    added poorly chosen reference genes

--- gene-counter.py
+++ gene-counter.py
@@ -6,7 +6,7 @@ from itertools import accumulate
 # Constants
 GENE_LIST_PATH = "./data/pca-circadian-filtering-top-genes-ensdargID.txt"
 DATA_PATH = "./data/all_sample_reads.csv"
-MIN_READS = 3000
+MIN_READS = 100
 
 # reads all the gene names in GENE_LIST_PATH, priority is the order that the first instance appears.
 # Tries to add genes in order of priority, conditional on every gene in the list being predicted atleast MIN_READS reads.
@@ -17,21 +17,13 @@ def get_favorite_ids(filepath):
         return dict.fromkeys(re.findall(r"ENSDARG\d{11}", f.read()))
 
 
-def try_int(value):
-    try:
-        return int(value)
-    except (ValueError, TypeError):
-        return 0
-
-
 def calulate_inclusion(gene_dict, grand_total):
     items = list(gene_dict.items())
-    keys, values = zip(*items)
     current_total = 0
-    min_gene = keys[0]
-    minimum = values[0]
+    min_gene = items[0][0]
+    minimum = items[0][1]
     genes, counts = [], []
-    for gene_id, total in gene_dict.items():
+    for gene_id, total in items:
         current_total += total
         new_min = min(minimum, total)
         if new_min * (grand_total / current_total) >= MIN_READS:
@@ -43,7 +35,9 @@ def calulate_inclusion(gene_dict, grand_total):
         else:
             current_total -= total
 
-    print(f"You should include the following {len(genes)} genes:")
+    print(
+        f"You should include the following {len(genes)} genes so that every gene is predicted to be read at least {MIN_READS} times:"
+    )
     for i in range(len(genes)):
         print(f"{genes[i]} {counts[i]}")
     print(
@@ -58,17 +52,14 @@ def process_gene_data(gene_list, csv_path):
     with open(csv_path, mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         grand_total = 0
-        num_genes = 0
-        for row in reader:
+        for count, row in enumerate(reader):
             gene_id = row.get("geneID")
-            total = sum(try_int(v) for k, v in row.items() if k != "geneID")
+            total = sum(int(v) if v.isdigit() else 0 for k, v in row.items())
             grand_total += total
-            num_genes += 1
             if gene_id in results:
-                total = sum(try_int(v) for k, v in row.items() if k != "geneID")
                 results[gene_id] = total
                 found_genes.add(gene_id)
-    print(f"There are a total of {grand_total} reads accross all {num_genes} genes")
+    print(f"There are a total of {grand_total} reads accross all {count + 1} genes")
     calulate_inclusion(results, grand_total)
 
 
