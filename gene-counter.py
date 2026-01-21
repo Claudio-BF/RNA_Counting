6bc4e1f1c822fd4506abeacabfd0efce85995266
Claudio-BF <claudiobf@uchicago.edu>
2026-01-20 20:02:57 -0600

    simplified logica

--- gene-counter.py
+++ gene-counter.py
@@ -9,8 +9,7 @@ DATA_PATH = "./data/all_sample_reads.csv"
 MIN_READS = 10
 
 # reads all the gene names in GENE_LIST_PATH, priority is the order that the first instance appears.
-# Will remove genes in reverse order of priority until the gene with the lowest number of reads in the current list is projected to be read at least MIN_READS times.
-# After that, adds as many genes as possible which which have more reads than the gene in list with the minimum number of reads.
+# Tries to add genes in order of priority, conditional on every gene in the list being predicted atleast MIN_READS reads.
 
 
 def get_favorite_ids(filepath):
@@ -29,42 +28,29 @@ def calulate_inclusion(gene_dict):
     items = list(gene_dict.items())
     keys, values = zip(*items)
     grand_total = sum(values)
+    current_total = 0
+    min_gene = keys[0]
+    minimum = values[0]
+    genes = []
+    counts = []
+    for gene_id, total in gene_dict.items():
+        current_total += total
+        new_min = min(minimum, total)
+        if new_min * (grand_total / current_total) >= 10:
+            genes.append(gene_id)
+            counts.append(total)
+            if total <= minimum:
+                minimum = new_min
+                min_gene = gene_id
+        else:
+            current_total -= total
 
-    # pre-calculate stats in optimal time complexity (lazy premade library solution)
-    totals = list(accumulate(values))
-    mins = list(accumulate(values, func=min))
-
-    # iterate backwards through pre-calculated stats
-    for i in range(len(items) - 1, -1, -1):
-        minimum = mins[i]
-        current_total = totals[i]
-        if minimum * (grand_total / current_total) >= 10:
-            final_idx = i
-            min_gene = keys[mins.index(minimum)]
-
-            print(f"You can include up to the first {final_idx + 1} genes.")
-            print(
-                f"Gene {min_gene} has the least reads among these with {minimum} reads."
-            )
-            print(
-                f"{min_gene} is projected to have {minimum * grand_total / current_total:.2f} reads when reading only these genes."
-            )
-            # figure out the other genes you can include
-            max_total = minimum * grand_total / 10.0
-            other_genes = []
-            for j in range(i + 1, len(values)):
-                if values[j] >= minimum:
-                    current_total += values[j]
-                    if current_total >= max_total:
-                        current_total -= values[j]
-                        break
-                    else:
-                        other_genes.append(keys[j])
-            print(
-                f"You might also include the following genes, at which point {min_gene} is projected to have {minimum * grand_total / current_total:.2f} reads"
-            )
-            print(*other_genes, sep="\n")
-            break
+    print(f"You should include the following {len(genes)} genes:")
+    for i in range(len(genes)):
+        print(f"{genes[i]} {counts[i]}")
+    print(
+        f"When reading these genes {min_gene} has the minimal number of reads ({minimum}) and is projected to have {minimum * grand_total / current_total:.2f} reads when only reading these genes"
+    )
 
 
 def process_gene_data(gene_list, csv_path):
@@ -79,10 +65,6 @@ def process_gene_data(gene_list, csv_path):
                 total = sum(try_int(v) for k, v in row.items() if k != "geneID")
                 results[gene_id] = total
                 found_genes.add(gene_id)
-
-    for gene_id, total in results.items():
-        if gene_id in found_genes:
-            print(f"{gene_id} {total}")
     calulate_inclusion(results)
 
 
